bc645fddf12a5257d94798bbcadbeeb208d992c0
FloRad <florian.radlberger@gmail.com>
2020-01-24 15:26:29 +0100

    WiP changes

--- src/module/character-sheet.ts
+++ src/module/character-sheet.ts
@@ -30,16 +30,32 @@ export class WildcardSheet extends ActorSheet {
       initial: initial,
       callback: clicked => this._sheetTab = clicked.data("tab")
     });
-
-    // Everything below here is only needed if the sheet is editable
-    if (!this.options.editable) return;
   }
 
   getData() {
     const data = super.getData();
     // Add any special data that your template needs here.
+  
     return data;
   }
+
+  determineRank(xp: number): String {
+    let retVal: String
+
+    if (xp <= 19) {
+      retVal = "Novice";
+    } else if (xp >= 20 && xp <= 39) {
+      retVal = "Seasoned";
+    } else if (xp >= 40 && xp < 59) {
+      retVal = "Veteran";
+    } else if (xp >= 60 && xp <= 79) {
+      retVal = "Heroic";
+    } else if (xp >= 80) {
+      retVal = "Legendary";
+    }
+    return retVal;
+  }
+
 }
 
 export class ExtraSheet extends ActorSheet {
