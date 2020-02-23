5bbcacf95443415b01d982d5241315826337da16
FloRad <florad92@gmail.com>
2020-02-23 15:12:21 +0100

    Broke up Wildcard sheet into reusable partials

--- src/module/preloadTemplates.ts
+++ src/module/preloadTemplates.ts
@@ -1,4 +1,4 @@
-export const preloadHandlebarsTemplates = async function() {
+export const preloadHandlebarsTemplates = async function () {
 	const templatePaths = [
 		// Add paths to "systems/swade/templates"
 		//Character Sheets
@@ -6,6 +6,12 @@ export const preloadHandlebarsTemplates = async function() {
 		'systems/swade/templates/actors/extra-sheet.html',
 
 		//Actor partials
+		//Sheet tabs
+		'systems/swade/templates/actors/partials/summary-tab.html',
+		'systems/swade/templates/actors/partials/edges-tab.html',
+		'systems/swade/templates/actors/partials/inventory-tab.html',
+
+		//Gear Cards
 		'systems/swade/templates/actors/partials/weapon-card.html',
 		'systems/swade/templates/actors/partials/armor-card.html',
 		'systems/swade/templates/actors/partials/shield-card.html',
