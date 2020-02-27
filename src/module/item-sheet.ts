6161cea896902d8a5653b6b5d10755b7a116fbfe
FloRad <florad92@gmail.com>
2020-02-27 21:13:23 +0100

    added missing data attribute

--- src/module/item-sheet.ts
+++ src/module/item-sheet.ts
@@ -28,9 +28,9 @@ export class SwadeItemSheet extends ItemSheet {
 
     // Delete Item from within Sheet. Only really used for skills Edges and Hindrances
     html.find('.item-delete').click(ev => {
-      const li = $(ev.currentTarget).parents('.sheet-body');
+      const item = $(ev.currentTarget).parents('.sheet-body');
       if (this.item.actor) {
-        this.item.actor.deleteOwnedItem(li.data('itemId'));
+        this.item.actor.deleteOwnedItem(item.data('itemId'));
         this.item.sheet.close();
       }
     });
