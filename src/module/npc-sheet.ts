e7c5c82d1c1c612dcccd877815197b5acf337d20
mesfoliesludiques <uman@mesfoliesludiques.fr>
2020-06-07 13:28:38 +0200

    FIX: margin not consistent on NPC sheet - Added also an edit button on npc inventory

--- src/module/npc-sheet.ts
+++ src/module/npc-sheet.ts
@@ -115,6 +115,12 @@ export class SwadeNPCSheet extends ActorSheet {
     const html = await super._renderInner(...args);
     this.form = html[0];
     
+    // Resize resizable classes
+    let resizable = (html as JQuery).find('.resizable');
+    resizable.each((_, el) => {
+      let heightDelta = this.position.height - (this.options.height as number);
+      el.style.height = `${heightDelta + parseInt(el.dataset.baseSize)}px`;
+    });
     // Filter power list
     const arcane = !this.options.activeArcane ? 'All' : this.options.activeArcane;
     (html as JQuery).find('.arcane-tabs .arcane').removeClass('active');
@@ -313,6 +319,16 @@ export class SwadeNPCSheet extends ActorSheet {
       return data;
     }
 
+    async _onResize(event: any) {
+      super._onResize(event);
+      let html = $(event.path);
+      let resizable = html.find('.resizable');
+      resizable.each((_, el) => {
+        let heightDelta = this.position.height - (this.options.height as number);
+        el.style.height = `${heightDelta + parseInt(el.dataset.baseSize)}px`;
+      });
+    }
+    
     private _checkNull(items: Item[]): any[] {
         if (items && items.length) {
             return items;
