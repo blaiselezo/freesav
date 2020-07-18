34d29b07715631cb97296ceb6a2ad240c2f5dc6c
FloRad <florad92@gmail.com>
2020-07-18 20:39:53 +0200

    removed two way icon binding

--- src/module/config.ts
+++ src/module/config.ts
@@ -46,16 +46,6 @@ export class SWADE {
     height: 300,
   };
 
-  public static bennies = {
-    templates: {
-      refresh: 'systems/swade/templates/chat/benny-refresh.html',
-      refreshAll: 'systems/swade/templates/chat/benny-refresh-all.html',
-      add: 'systems/swade/templates/chat/benny-add.html',
-      spend: 'systems/swade/templates/chat/benny-spend.html',
-      gmadd: 'systems/swade/templates/chat/benny-gmadd.html',
-    },
-  };
-
   public static vehicles = {
     maxHandlingPenalty: -4,
   };
