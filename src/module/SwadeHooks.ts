b63a717b5f0ff97a8572d8511efc8e33911bc745
FloRad <florad92@gmail.com>
2021-02-07 18:18:58 +0100

    fixed actor check on reroll

--- src/module/SwadeHooks.ts
+++ src/module/SwadeHooks.ts
@@ -412,7 +412,7 @@ export default class SwadeHooks {
         message?.isRoll &&
         message?.isContentVisible &&
         !message.getFlag('core', 'RollTable');
-      return isRightMessageType && (game.user.isGM || actor.owner);
+      return isRightMessageType && !!actor && (game.user.isGM || actor.owner);
     };
     options.push(
       {
